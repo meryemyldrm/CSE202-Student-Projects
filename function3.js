a849d324f3a9c687f290269cddf657f75d195fc9
meryemyldrm <133081378+meryemyldrm@users.noreply.github.com>
2023-05-10 14:30:55 +0300

    Update function3.js
    Signed-off-by: meryemyldrm <133081378+meryemyldrm@users.noreply.github.com>

---
+++ function3.js
@@ -0,0 +1,10 @@
+function filterEven(arr) {
+  const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+}
+console.log(filterEven(arr));

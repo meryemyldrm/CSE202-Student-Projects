23435f3f8f77da2f245c0cf0c019639a66798fa7
meryemyldrm <133081378+meryemyldrm@users.noreply.github.com>
2023-05-10 14:48:40 +0300

    function5.js
    Signed-off-by: meryemyldrm <133081378+meryemyldrm@users.noreply.github.com>

---
+++ function5.js
@@ -0,0 +1,7 @@
+const arr = [4, 9, 16, 25];
+const sqrtArr = arr.map((num) => Math.sqrt(num));
+console.log(sqrtArr); // Output: [2, 3, 4, 5]
+
+const arr = [1, 2, 3, 4, 5];
+const multipliedArr = arr.map((num) => num * 2);
+console.log(multipliedArr);
